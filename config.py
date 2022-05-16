a45a0ad5c1343cc9b7f5c76b4bf9076971a72d4d
cassie003 <caseyminu@gmail.com>
2022-05-16 10:41:20 +0300

    Heroku push

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
+    SQLALCHEMY_DATABASE_URI = 'postgres://qgrgctfzlcxizb:f2168f6978e44c4b28e2628724ab046812755f1c25272b330bcd74bd4decac7e@ec2-3-224-164-189.compute-1.amazonaws.com:5432/d6s9gcq551hrrj'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS=True
     SECRET_KEY=os.environ.get('SECRET_KEY')
